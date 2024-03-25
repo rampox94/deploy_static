dd78e164c92e564aa1a02069ab66bcdb03cbc8b8
Valerio Rampa <rampavalerio@gmail.com>
2024-03-25 15:24:19 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     port: 8080,
     hot: true,
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
   module: {
     rules: [
       {
